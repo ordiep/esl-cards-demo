137e0ab6d89aeb0ffa96fe23bc4e21eda1b373f4
Ordie Page <opage@Ordies-iMac.local>
2020-05-04 03:17:05 -0500

    Added Data Fetching & Basic CSS

--- src/js/components/esl-card.js
+++ src/js/components/esl-card.js
@@ -1,8 +1,17 @@
 import React from 'react';
 const Card = (props) => {
+  const { cardInfo } = props;
+  const { imageUrl, name, text, type  } = cardInfo;
+  const setName = cardInfo.set.name;
+  const altText = `Elder Scroll Legends Card Image For ${name}`;
+  
   return (
     <div className="esl-card">
-      This is Card #{props.id}
+      <h2>{name}</h2>
+      <p><b>Type: </b>{type}</p>
+      <p><b>Set:</b> {setName}</p>
+      <img src={imageUrl} alt={altText}/>
+      <p><b>Description:</b> {text}</p>
     </div>
   )
 }
--- src/js/components/esl-grid.js
+++ src/js/components/esl-grid.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './esl-card';
+
+
 const Grid = (props) => {
-  const cards = Array(20).fill(0).map( (value, index) =>  <Card key={index} id={index + 1} />);
+  const [cardState, setCardState] = useState({cards: []});
+  const [isLoading, setLoading] = useState(true);
+  const [pageNumber, setPageNumber] = useState(1);
+  const loader = <div className="loader"></div>;
+  const cards = cardState.cards.map((cardData, index) =>{
+    return (<Card key={index + 1} cardInfo={cardData} />);
+  });
+
+  
+  async function fetchCardData(pageNumber) {
+    let response = await fetch(`https://api.elderscrollslegends.io/v1/cards?pageSize=20&page=${pageNumber}`);
+    let data = await response.json();
+    setCardState({cards: cardState.cards.concat(data.cards) });
+    setLoading(false);
+  }
+  
+  useEffect(() =>{
+    const grid = document.getElementsByClassName('esl-grid')[0];
+    window.addEventListener('scroll', () => {
+      if (window.scrollY + window.innerHeight >= grid.clientHeight + grid.offsetTop) {
+        setLoading(true);
+        setPageNumber(pageNumber + 1);
+      }
+    });
+    fetchCardData(pageNumber);
+  }, [pageNumber]);
+
   return (
     <div className="esl-grid">
-      {cards}
+      { cards.length ? cards : false }
+      { isLoading ? loader : false }
     </div>
   )
 }
-export default Grid;
+  export default Grid;
